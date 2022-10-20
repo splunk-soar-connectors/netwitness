b541761748dc1d31825393e3e1ab6c16d2bb5472
mishalp-crest <70274577+mishalp-crest@users.noreply.github.com>
2022-10-20 10:43:32 +0530

    PAPP-27139: Updated REST handler for better handling of optional parameters (#5)
    * Updated REST handler for better handling of optional parameters
    
    * pre-commit changes
    
    * Semgrep pipline changes
    
    * Updated app_version
    
    * Reverted minor changes
    
    * latest pre commit run
    
    * Added error_print
    
    * updated min_phantom_v ersion
    
    * Added table for upload file and restart device
    
    * Added message in _restart_device and added exception object in error print
    
    * changes in upload file's column name

--- netwitness_consts.py
+++ netwitness_consts.py
@@ -30,6 +30,7 @@ NETWITNESS_REPORT_ALREADY_AVAILABLE = 'Report already available in vault'
 NETWITNESS_CAP_TYPE_DICT = {'log': 'application/json', 'pcap': 'pcap'}
 NETWITNESS_SUCC_FILE_ADD_TO_VAULT = 'Successfully added file to Vault'
 NETWITNESS_SUCC_UPLOAD = 'Feed/Parser file successfully uploaded'
+NETWITNESS_SUCC_RESTART = 'Device successfully restarted'
 NETWITNESS_FILE_TYPE_DICT = {'pcap': 'pcap', 'log': 'json'}
 NETWITNESS_INVALID_PARAM = 'Invalid parameters: {message}'
 NETWITNESS_EXCEPTION_OCCURRED = 'Exception occurred'
@@ -50,7 +51,7 @@ NETWITNESS_TEST_CONNECTIVITY_FAIL = 'Connectivity test failed'
 
 NETWITNESS_CONFIG_VERIFY = 'verify_server_cert'
 NETWITNESS_CONFIG_API_USERNAME = 'username'
-NETWITNESS_CONFIG_API_PASSWORD = 'password'
+NETWITNESS_CONFIG_API_PASSWORD = 'password'  # pragma: allowlist secret
 NETWITNESS_CONFIG_SERVER = 'url'
 
 NETWITNESS_JSON_START_TIME = 'start_time'
@@ -65,8 +66,11 @@ NETWITNESS_DEFAULT_TEST_TIMEOUT = 30
 NETWITNESS_CAP_TYPE_PACKET = 'pcap'
 NETWITNESS_CAP_TYPE_LOG = 'log'
 
-NETWITNESS_BAD_CAP_HASHES = [ '2d59f05b63bda3d74db5685834e137911c7279f282f0d595ac465fd3b97e348f',
-                              '8eba672603c531f466c75ca729b66378b92271d78ef1570574757cd5fd8244e6']
+NETWITNESS_BAD_CAP_HASHES = [ '2d59f05b63bda3d74db5685834e137911c7279f282f0d595ac465fd3b97e348f',  # pragma: allowlist secret
+                              '8eba672603c531f466c75ca729b66378b92271d78ef1570574757cd5fd8244e6']  # pragma: allowlist secret
 
 # Updating bad CAP sizes for Python 3 support
 NETWITNESS_BAD_CAP_SIZES = [ 73, 37 ]
+
+# Constants relating to '_get_error_message_from_exception'
+NETWITNESS_ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
